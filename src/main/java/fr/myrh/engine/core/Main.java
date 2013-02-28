6cfaa44e61fcf6063e4886640c53940f92e51382
rgba <martin@myrh.fr>
2013-02-28 21:05:36 +0100

    + .gitignore for java files + main java

---
+++ src/main/java/fr/myrh/engine/core/Main.java
@@ -0,0 +1,16 @@
+package fr.myrh.engine.core;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rgba
+ * Date: 2/28/13
+ * Time: 8:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("test");
+  }
+
+}
